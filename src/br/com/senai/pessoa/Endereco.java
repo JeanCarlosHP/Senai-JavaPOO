9a61de119bf5ec366d2a2d27d58faab9679bf280
JeanCarlosHP <jeanchpaula@gmail.com>
2021-05-31 18:22:55 -0300

    Definição da classe Pessoa e Endereço

---
+++ src/br/com/senai/pessoa/Endereco.java
@@ -0,0 +1,30 @@
+package br.com.senai.pessoa;
+
+public class Endereco {
+	
+	// Atributos
+	private String cidade;
+	private String estado;
+	private String pais;
+	
+	// Métodos
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+}
---
+++ src/br/com/senai/pessoa/Pessoa.java
@@ -0,0 +1,43 @@
+package br.com.senai.pessoa;
+
+import java.util.Calendar;
+
+// Classe
+public class Pessoa extends Endereco {
+	
+	// Atributos	
+	private String nome;
+	private int anoNascimento;
+	private int idade;
+	private double altura;
+	
+	// Métodos
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getAnoNascimento() {
+		return anoNascimento;
+	}
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	private void setIdade() {
+		Calendar calendar = Calendar.getInstance();
+		
+		this.idade = calendar.get(Calendar.YEAR) - getAnoNascimento();
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+}
