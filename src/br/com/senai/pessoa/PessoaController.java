22301e98b8b2959ae7d3545f773ea0cf97893094
JeanCarlosHP <jeanchpaula@gmail.com>
2021-06-07 16:31:57 -0300

    Métodos de adição e listagem de dados do objeto pessoa

--- src/br/com/senai/pessoa/PessoaController.java
+++ src/br/com/senai/pessoa/PessoaController.java
@@ -1,7 +1,65 @@
 package br.com.senai.pessoa;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class PessoaController {
 	
+	private Scanner in;
 	
-
+	public PessoaController() {
+		in = new Scanner(System.in);
+	}
+	
+	public int leOpcao() {
+		System.out.print("> ");
+		return in.nextInt();
+	}
+	
+	public void menu() {
+		System.out.println("\n------- MENU -------");
+		System.out.println("1 - Cadastrar pessoa");
+		System.out.println("2 - Listar pessoas cadastradas");
+		System.out.println("9 - Sair do sistema");
+		System.out.println("--------------------");
+	}
+	
+	public Pessoa cadastrarPessoa() {
+		Pessoa pessoa = new Pessoa();
+		
+		System.out.println("\n--- CADASTRAR PESSOA ---");
+		
+		in.nextLine();
+		
+		System.out.print("Informe o nome: ");
+		pessoa.setNome(in.nextLine());
+		
+		System.out.print("Informe o ano de nascimento: ");
+		pessoa.setAnoNascimento(in.nextInt());
+		
+		System.out.print("Informe a altura: ");
+		pessoa.setAltura(in.nextDouble());
+		
+		System.out.println("\nPessoa cadastrada");
+		
+		return pessoa;
+	}
+	
+	public List<Pessoa> listarPessoas(List<Pessoa> pessoas) {
+		System.out.println("--- PESSOAS CADASTRADAS ---");
+		
+		System.out.printf("| %20s | %4s | %5s | %6s |\n", "Nome", "Ano", "Idade", "Altura");
+		
+		for(int i = 0; i < pessoas.size(); i++) {
+			System.out.printf("| %20s | %4d | %5d | %6.2f |\n",
+					pessoas.get(i).getNome(),
+					pessoas.get(i).getAnoNascimento(),
+					pessoas.get(i).getIdade(),
+					pessoas.get(i).getAltura()
+					);
+		}
+		
+		return pessoas;
+	}
+	
 }
