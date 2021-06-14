cf7a683a5213238060a7e0c033b4ce161050e752
JeanCarlosHP <jeanchpaula@gmail.com>
2021-06-14 16:41:10 -0300

    Refatoração de classe

--- src/br/com/senai/ProgramaPrincipal.java
+++ src/br/com/senai/ProgramaPrincipal.java
@@ -1,22 +1,32 @@
-package br.com.senai.pessoa;
+package br.com.senai;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import br.com.senai.loja.VendaController;
+import br.com.senai.pessoa.Pessoa;
+import br.com.senai.pessoa.PessoaController;
+import br.com.senai.produto.Produto;
+import br.com.senai.produto.ProdutoController;
 
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 
 		List<Pessoa> pessoas = new ArrayList<>();
+		List<Produto> produtos = new ArrayList<Produto>();
+		
+		Menu menu = new Menu();
 
 		PessoaController pessoaController = new PessoaController();
+		ProdutoController produtoController = new ProdutoController();
 
 		boolean sair = false;
 
 		do {
-			pessoaController.menu();
+			menu.menu();
 
-			int opcao = pessoaController.leOpcao();
+			int opcao = menu.leOpcao();
 
 			switch (opcao) {
 			case 1:
@@ -25,6 +35,14 @@ public class ProgramaPrincipal {
 
 			case 2:
 				pessoaController.listarPessoas(pessoas);
+				break;
+			
+			case 3:
+				produtos.add(produtoController.adicionarProduto());
+				break;
+			
+			case 4:
+				produtoController.listarProdutos(produtos);
 				break;
 
 			case 9:
