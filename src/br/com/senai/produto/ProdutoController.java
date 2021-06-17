5140ddaabf2a218fe94000e27f55fa1d0f193eaa
JeanCarlosHP <jeanchpaula@gmail.com>
2021-06-17 16:36:19 -0300

    Add o método editarProduto

--- src/br/com/senai/produto/ProdutoController.java
+++ src/br/com/senai/produto/ProdutoController.java
@@ -19,13 +19,14 @@ public class ProdutoController {
 		System.out.println("\n--- ADICIONAR PRODUTO ---");
 
 		System.out.print("Informe o nome do produto: ");
-		produto.setNomeDoProduto(in.nextLine());
+		produto.setNomeDoProduto(in.next());
 		
 		System.out.print("Informe o valor unitário do produto: ");
 		produto.setValorUnitarioDoProduto(in.nextDouble());
 		
 		System.out.print("Informe a quantidade do produto: ");
 		produto.setQuantidadeDoProduto(in.nextInt());
+		produto.setValorTotalDoProduto(produto.getValorUnitarioDoProduto() * produto.getQuantidadeDoProduto());
 		
 		System.out.println("\nProduto adicionado");
 
@@ -35,11 +36,12 @@ public class ProdutoController {
 	public List<Produto> listarProdutos(List<Produto> produtos) {
 		System.out.println("\n--- PRODUTOS ADICIONADOS ---");
 		
-		System.out.printf("| %10s | %14s | %10s | %11s |\n", 
-				"Nome", "Valor Unitário", "Quantidade", "Valor Total");
+		System.out.printf("| %2s | %4s | %14s | %10s | %11s |\n", 
+				"Id", "Nome", "Valor Unitário", "Quantidade", "Valor Total");
 
 		for (int i = 0; i < produtos.size(); i++) {
-			System.out.printf("| %10s | %14.2f | %10d | %11.2f |\n", 
+			System.out.printf("| %2d | %4s | %14.2f | %10d | %11.2f |\n", 
+					i,
 					produtos.get(i).getNomeDoProduto(),
 					produtos.get(i).getValorUnitarioDoProduto(), 
 					produtos.get(i).getQuantidadeDoProduto(),
@@ -48,6 +50,77 @@ public class ProdutoController {
 		}
 		
 		return produtos;
+	}
+	
+	public List<Produto> editarProduto(List<Produto> produtos) {
+		
+		Produto produto = new Produto();
+		
+		listarProdutos(produtos);
+		
+		System.out.print("\nInforme o Id do produto para editar: ");
+		int idProduto = in.nextInt();
+		
+		System.out.println("\n1) Editar o nome do produto");
+		System.out.println("2) Editar o valor unitário do produto");
+		System.out.println("3) Editar a quantidade do produto");
+		
+		System.out.print("\nInforme o campo para ser editado: ");
+		int opcao = in.nextInt();
+		
+		switch (opcao) {
+		case 1:
+			System.out.println("\n--- EDITAR O NOME O PRODUTO ---");
+			
+			System.out.print("Informe o novo nome do produto: ");
+			produto.setNomeDoProduto(in.next());
+			
+			produto.setQuantidadeDoProduto(produtos.get(idProduto).getQuantidadeDoProduto());
+			produto.setValorUnitarioDoProduto(produtos.get(idProduto).getValorUnitarioDoProduto());
+			produto.setValorTotalDoProduto(produtos.get(idProduto).getValorTotalDoProduto());
+			
+			produtos.set(idProduto, produto);
+			
+			System.out.println("\nNome alterado!");
+			break;
+		
+		case 2:
+			System.out.println("\n--- EDITAR O VALOR UNITÁRIO DO PRODUTO ---");
+			
+			System.out.print("Informe o novo valor unitário do produto: ");
+			produto.setValorUnitarioDoProduto(in.nextDouble());
+			
+			produto.setNomeDoProduto(produtos.get(idProduto).getNomeDoProduto());
+			produto.setQuantidadeDoProduto(produtos.get(idProduto).getQuantidadeDoProduto());
+			produto.setValorTotalDoProduto(produto.getValorUnitarioDoProduto() * produto.getQuantidadeDoProduto());
+			
+			produtos.set(idProduto, produto);
+			
+			System.out.println("\nValor unitário alterado!");
+			break;
+			
+		case 3:
+			System.out.println("\n--- EDITAR A QUANTIDADE DO PRODUTO ---");
+			
+			System.out.print("Informe a nova quantidade do produto: ");
+			produto.setQuantidadeDoProduto(in.nextInt());
+			
+			produto.setNomeDoProduto(produtos.get(idProduto).getNomeDoProduto());
+			produto.setValorUnitarioDoProduto(produtos.get(idProduto).getValorUnitarioDoProduto());
+			produto.setValorTotalDoProduto(produto.getValorUnitarioDoProduto() * produto.getQuantidadeDoProduto());
+			
+			produtos.set(idProduto, produto);
+			
+			System.out.println("\nQuantidade alterada!");
+			break;
+			
+		default:
+			System.out.println("Opção Inválida!");
+			break;
+		}
+		
+		return produtos;
+		
 	}
 
 }
