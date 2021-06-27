3712e16d58546242da088353fbfc7b81f6b09ffe
JeanCarlosHP <jeanchpaula@gmail.com>
2021-06-27 14:56:19 -0300

    Refatoração na chamada dos métodos dos cases

--- src/br/com/senai/ProgramaPrincipal.java
+++ src/br/com/senai/ProgramaPrincipal.java
@@ -6,13 +6,15 @@ import java.util.List;
 import br.com.senai.loja.VendaController;
 import br.com.senai.pessoa.Pessoa;
 import br.com.senai.pessoa.PessoaController;
+import br.com.senai.produto.Produto;
 import br.com.senai.produto.ProdutoController;
 
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 		List<Pessoa> pessoas = new ArrayList<>();
-		
+		List<Produto> produtos = new ArrayList<Produto>();
+
 		Menu menu = new Menu();
 		PessoaController pessoaController = new PessoaController();
 		ProdutoController produtoController = new ProdutoController();
@@ -22,28 +24,28 @@ public class ProgramaPrincipal {
 
 		do {
 			menu.menuPrincipal();
-			
+
 			int opcao = menu.leOpcao();
 
 			switch (opcao) {
-			case 1:
-				pessoaController.menuPessoa(pessoas);
-				break;
+				case 1:
+					pessoaController.menuPessoa(pessoas);
+					break;
 
-			case 2:
-				produtoController.menuProduto();
-				break;
-			
-			case 3:
-				vendaController.menuVenda();
-				break;
+				case 2:
+					produtoController.menuProduto(produtos);
+					break;
 
-			case 0:
-				sair = true;
-				break;
+				case 3:
+					vendaController.menuVenda(pessoas, produtos);
+					break;
 
-			default:
-				System.out.println("\nOpÁ„o inv·lida!");
+				case 0:
+					sair = true;
+					break;
+
+				default:
+					System.out.println("\nOp√ß√£o inv√°lida!");
 			}
 
 		} while (!sair);
